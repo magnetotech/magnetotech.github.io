07e85646a52893e01af46f063fac7286ec61d509
James Hall <thatwhichendureth@icloud.com>
2016-12-10 19:49:03 -0500

    new file:   articles/index.html 	new file:   articles/install-arch-linux/index.html 	new file:   css/custom.css 	new file:   css/normalize.css 	new file:   css/skeleton.css 	new file:   home/index.html 	new file:   img/favicon.ico 	new file:   img/favicon1.ico 	new file:   img/tux.png 	new file:   js/ahead.js 	new file:   js/head.html 	new file:   js/head.js

---
+++ js/ahead.js
@@ -0,0 +1,24 @@
+document.write("<div class=\"mdl-layout mdl-js-layout mdl-layout--fixed-header\">");
+document.write("  <header class=\"mdl-layout__header\">");
+document.write("    <div class=\"mdl-layout__header-row\">");
+document.write("      <!-- Title -->");
+document.write("      <span class=\"mdl-layout-title\" style=\"font-family: 'Special Elite', cursive;\"><a href=\"..\/..\/home\/index.html\" style=\"text-decoration: none; color: white;\">Magneto Tech<\/a><\/span>");
+document.write("      <!-- Add spacer, to align navigation to the right -->");
+document.write("      <div class=\"mdl-layout-spacer\"><\/div>");
+document.write("      <!-- Navigation. We hide it in small screens. -->");
+document.write("      <nav class=\"mdl-navigation mdl-layout--large-screen-only\">");
+document.write("        <a class=\"mdl-navigation__link\" href=\"..\/..\/articles\/index.html\">Articles<\/a>");
+document.write("        <a class=\"mdl-navigation__link\" href=\"..\/..\/about\/index.html\">About<\/a>");
+document.write("      <a class=\"mdl-navigation__link\" href=\"..\/..\/Donate\/index.html\">Donate<\/a>");
+document.write("      <\/nav>");
+document.write("    <\/div>");
+document.write("  <\/header>");
+document.write("  <div class=\"mdl-layout__drawer\">");
+document.write("      <span class=\"mdl-layout-title\" style=\"font-family: 'Special Elite', cursive;\"><a href=\"..\/..\/home\/index.html\" style=\"text-decoration: none; color: black;\">Magneto Tech<\/a><\/span>");
+document.write("    <nav class=\"mdl-navigation\">");
+document.write("      <a class=\"mdl-navigation__link\" href=\"..\/..\/articles\/index.html\">Articles<\/a>");
+document.write("      <a class=\"mdl-navigation__link\" href=\"..\/..\/about\/index.html\">About<\/a>");
+document.write("      <a class=\"mdl-navigation__link\" href=\"..\/..\/donate\/index.html\">Donate<\/a>");
+document.write("    <\/nav>");
+document.write("  <\/div>");
+
